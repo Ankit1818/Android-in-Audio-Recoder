a802707f418a39965a27f4b82c3194b0aad9e980
Ankit Goswami <53818978+Ankit1818@users.noreply.github.com>
2020-01-27 02:07:25 -0800

    Add files via upload
    Re coder

---
+++ src/main/java/com/example/androidrecoder/MainActivity.java
@@ -0,0 +1,107 @@
+package com.example.androidrecoder;
+
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.os.Environment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
+import static android.Manifest.permission.RECORD_AUDIO;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+public class MainActivity extends AppCompatActivity {
+    private Button startbtn, stopbtn, playbtn, stopplay;
+    private MediaRecorder mRecorder;
+    private MediaPlayer mPlayer;
+    private static String mFileName = null;
+    public static final int REQUEST_AUDIO_PERMISSION_CODE = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startbtn = (Button)findViewById(R.id.btnRecord);
+        stopbtn = (Button)findViewById(R.id.btnStop);
+        playbtn = (Button)findViewById(R.id.btnPlay);
+        stopplay = (Button)findViewById(R.id.btnStopPlay);
+        stopbtn.setEnabled(false);
+        playbtn.setEnabled(false);
+        stopplay.setEnabled(false);
+        mFileName = Environment.getExternalStorageDirectory().getAbsolutePath() + "/AudioRecording.3gp";
+
+        startbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                    stopbtn.setEnabled(true);
+                    startbtn.setEnabled(false);
+                    playbtn.setEnabled(false);
+                    stopplay.setEnabled(false);
+                    mRecorder = new MediaRecorder();
+                    mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+                    mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+                    mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+                    mRecorder.setOutputFile(mFileName);
+                    try {
+                        mRecorder.prepare();
+                    } catch (IOException e) {
+
+                    }
+                    mRecorder.start();
+                    Toast.makeText(getApplicationContext(), "Recording Started", Toast.LENGTH_LONG).show();
+
+            }
+        });
+        stopbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopbtn.setEnabled(false);
+                startbtn.setEnabled(true);
+                playbtn.setEnabled(true);
+                stopplay.setEnabled(true);
+                mRecorder.stop();
+                mRecorder.release();
+                mRecorder = null;
+                Toast.makeText(getApplicationContext(), "Recording Stopped", Toast.LENGTH_LONG).show();
+            }
+        });
+        playbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopbtn.setEnabled(false);
+                startbtn.setEnabled(true);
+                playbtn.setEnabled(false);
+                stopplay.setEnabled(true);
+                mPlayer = new MediaPlayer();
+                try {
+                    mPlayer.setDataSource(mFileName);
+                    mPlayer.prepare();
+                    mPlayer.start();
+                    Toast.makeText(getApplicationContext(), "Recording Started Playing", Toast.LENGTH_LONG).show();
+                } catch (IOException e) {
+
+                }
+            }
+        });
+        stopplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPlayer.release();
+                mPlayer = null;
+                stopbtn.setEnabled(false);
+                startbtn.setEnabled(true);
+                playbtn.setEnabled(true);
+                stopplay.setEnabled(false);
+                Toast.makeText(getApplicationContext(),"Playing Audio Stopped", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+}
